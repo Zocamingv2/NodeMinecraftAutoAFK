b7f1c72b365d4db6c8f58f8fbb940fff9f4ef47d
Zocamingv2 <77677179+Zocamingv2@users.noreply.github.com>
2022-06-11 00:33:59 +0300

    Update

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 //login config
 
-var server_host = '';
-var server_port = 25565;
-var username = '';
-var password = '';
-var auth = 'microsoft';
+var server_host = 'play.zocamc.tk';
+var server_port = 57402;
+var username = 'ZocaAdam';
+var password = 'zoca3162';
+var auth = 'mojang';
 //var auth = 'mojang';
 
 exports.server_host = server_host;
